56bacd39b9a46ee3f44902637aafe2632975b304
phegderi93 <pnirmalahegdeusf@gmail.com>
2025-03-10 13:57:08 -0400

    Update logger.py

--- reditt_api_functions/logger/logger.py
+++ reditt_api_functions/logger/logger.py
@@ -1,20 +1,44 @@
 import logging
 import os
-
+import boto3
+from configs.credentials import s3_credentials
+import shutil
 
 log_dir = 'logs'
+log_file = os.path.join(log_dir, "app.log")
 
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+def configure_logger():
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-# Configure the logger
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler(os.path.join(log_dir, "app.log")),
-        logging.StreamHandler()
-    ]
-)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
 
-logger = logging.getLogger(__name__)
+    return logging.getLogger(__name__)
+
+logger = configure_logger()
+
+def upload_log_to_s3():
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=s3_credentials['aws_access_key_id'],
+        aws_secret_access_key=s3_credentials['aws_secret_access_key']
+    )
+    s3_bucket = "prasad-reditt-test-log"
+    s3_prefix = "log_folder"
+    s3_key = f"{s3_prefix}/{os.path.basename(log_file)}"
+
+    try:
+        s3_client.upload_file(log_file, s3_bucket, s3_key)
+        logger.info(f"Log file uploaded to S3 at s3://{s3_bucket}/{s3_key}")
+        # Remove the log directory only if the upload is successful
+        if os.path.exists(log_dir):
+            shutil.rmtree(log_dir)
+    except boto3.exceptions.S3UploadFailedError as e:
+        logger.error(f"Failed to upload log file to S3: {e}")
